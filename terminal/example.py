ddf4328aefbd943fe393543851d3658153939a3e
Jaspar Löchte <jaspar.loechte@greenbone.net>
2022-01-05 11:24:29 +0100

    Update the example, using context for file, add example for indentation

--- terminal/example.py
+++ terminal/example.py
@@ -17,33 +17,46 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import terminal
+from pathlib import Path
 import requests
+
+import terminal
 
 def main(args=None):
     terminal.init_terminal(welcome=True)
+
+    terminal.out('')
+    # print an ok statement
     terminal.ok('Nice!')
 
+    with terminal.get_terminal().indent():
+        terminal.bold_info('This line is indented')
+
+    terminal.warning('This line is not indented anymore!')
+    terminal.out('')
+
+    # example for the loader
     url = "https://www.dundeecity.gov.uk/sites/default/files/publications/civic_renewal_forms.zip"
 
-    file_name = url.split('/')[-1]
+    file_name = Path(url.split('/')[-1])
     u = requests.get(url, stream=True, timeout=10000)
-    f = open(file_name, 'wb')
 
     file_size_dl = 0
     chunk_size = 8192
+    with open(file_name, 'wb') as f:
 
-    file_size = u.headers.get('content-length')
-    terminal.load_status(msg=file_name, total=int(file_size))
-    for content in u.iter_content(chunk_size=chunk_size):
+        file_size = u.headers.get('content-length')
+        terminal.progress(msg=file_name, total=int(file_size))
+        for content in u.iter_content(chunk_size=chunk_size):
 
-        file_size_dl += len(content)
-        f.write(content)
+            file_size_dl += len(content)
+            f.write(content)
 
-        terminal.load_status(current=file_size_dl)
-    terminal.load_status(done=True)
+            terminal.progress(current=file_size_dl)
+        terminal.progress(done=True)
 
-    f.close()
+    file_name.unlink()
+
 
 if __name__ == '__main__':
     main()
